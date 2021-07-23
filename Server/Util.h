77cb9fe022de76ee2d2040994969662f295f9530
Sangjin0 <qak6464@naver.com>
2021-07-23 21:43:33 +0900

    Initialize Gomoku Project

---
+++ Server/Util.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef GOMOKU_UTIL_H
+#define GOMOKU_UTIL_H
+using namespace std;
+#include <vector>
+#include <sstream>
+class Util {
+public:
+	vector<string> getTokens(string input, char delimiter);
+};
+#endif
